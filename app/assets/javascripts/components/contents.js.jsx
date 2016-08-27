fd22197fe697a11da9a59536021cb340596bedb2
yogoken <yogo.k@husky.neu.edu>
2016-08-27 23:32:41 +0900

    Add var More in contents.jsx

--- app/assets/javascripts/components/contents.js.jsx
+++ app/assets/javascripts/components/contents.js.jsx
@@ -12,15 +12,26 @@ var Contents = React.createClass({
 
 var ContentPost = React.createClass({
   render: function() {
-    var backgroundImage = { "background-image": "url(http://www.seplus.jp/sezemi/github/images/github.png)"}
+    var backgroundImage = { "backgroundImage": "url(http://www.seplus.jp/sezemi/github/images/github.png)"}
     return (
       <div className="content_post" style={backgroundImage}>
+        <More />
         ツイート内容
         <span className="name">
           <a>
             <span>投稿者</span>よごけん
           </a>
         </span>
+      </div>
+    );
+  }
+});
+
+var More = React.createClass({
+  render: function() {
+    var backgroundImage = { "backgroundImage": "url(https://bermuda-store.net/assets/bermuda_store/arrow_left-0b674062302023af70410061703455b0.png)"}
+    return (
+      <div className="more" style={backgroundImage}>
       </div>
     );
   }
